110e1c8015552ed91b0e6d160541087357b5dc96
Fathima N <fathima.n@skywalkglobal.net>
2025-11-19 17:39:31 +0530

    Add project files.

---
+++ CRUDAPPDEMO/DATA/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CRUDAPPDEMO.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDAPPDEMO.DATA
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options): base(options) 
+        {
+            
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ CRUDAPPDEMO/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUDAPPDEMO.Models
+{
+    public class Student
+    {
+        [Key]
+        [Display (Name = "Student ID")]
+        [Required]
+        public int Id { get; set; }
+        [Display(Name = "Student Name")]
+        [Required]
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Course { get; set; }
+    }
+}
